f8fe1537860c733411836a76d1f939daac501392
Timur <kyrilov.timur@gmail.com>
2024-07-27 21:48:01 +0300

    upgrade pip + rework html

--- server/search/views.py
+++ server/search/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .forms import SearchForm
 import requests
+from dotenv import load_dotenv
+import os
 
 # Create your views here.
 def weather_search(request):
@@ -10,7 +12,8 @@ def weather_search(request):
     if form.is_valid():
         search = form.cleaned_data.get('query', None)
         if search:
-            api_url = f'http://api.weatherapi.com/v1/current.json?key=a5cd732242114d0a93e140101240507&q={search}&aqi=no'
+            key = os.getenv('API_KEY')
+            api_url = f'http://api.weatherapi.com/v1/current.json?key={key}&q={search}&aqi=no'
             response = requests.get(api_url)
             if response.status_code == 200:
                 weather_data = response.json()
