1ffdf921148468f87541f4c32b2aba407f2d17fd
Timur <kyrilov.timur@gmail.com>
2024-07-28 10:47:22 +0300

    gitignore

--- server/content/views.py
+++ server/content/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+import os 
+import requests
+from dotenv import load_dotenv
+
+key = os.getenv('API_KEY')
 
 def main_page(request):
+    
     return render(request, 'content/main.html')
