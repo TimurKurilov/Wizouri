7fa99580a1663360fc91bd7cb1299b6c9f42e022
Timur <kyrilov.timur@gmail.com>
2024-08-13 20:32:03 +0300

    commit #0

--- server/search/views.py
+++ server/search/views.py
@@ -4,6 +4,8 @@ import requests
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 # Create your views here.
 def weather_search(request):
     form = SearchForm(request.GET)
@@ -14,7 +16,7 @@ def weather_search(request):
         if search:
             key = os.getenv('API_KEY')
             api_url = f'http://api.weatherapi.com/v1/current.json?key={key}&q={search}&aqi=no'
-            response = requests.get(api_url)
+            response = requests.get(api_url, verify=False)
             if response.status_code == 200:
                 weather_data = response.json()
     return render(request, 'search/weather_results.html', {'form': form, 'weather_data': weather_data, 'search' : search})
